e60e50a581e861b2007b74eba5f075dd87199997
Dorian Taing <doriantaing77@hotmail.fr>
2019-04-10 11:41:16 +0200

    init

---
+++ src/App/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../assets/style/style.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Happy Moments</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
